3d304816f14cd2918beb0956efa1df15d64c60e9
Matthew Garvey <47584607+matthewgarvey963@users.noreply.github.com>
2025-03-18 16:40:53 -0400

    Update config.js

--- static/config.js
+++ static/config.js
@@ -14,6 +14,6 @@
  * @type {{apiKey: string, authDomain: string}}
  */
 const config = {
-  apiKey: "AIzaSyDnIuYqov8WMy1wbGAL3_dS8bwduWNUmIs",
-  authDomain: "cs1660-project3.firebaseapp.com",
+  apiKey: 'AIzaSyDnIuYqov8WMy1wbGAL3_dS8bwduWNUmIs',
+  authDomain: 'cs1660-project3.firebaseapp.com',
 };
